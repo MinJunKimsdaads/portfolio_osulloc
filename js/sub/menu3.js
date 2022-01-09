fadea48c75c2016f285c053b92d63ebb3b99f3cb
MinJunKimsdaads <aaeexx@naver.com>
2022-01-09 23:04:53 +0900

    132

--- js/sub/menu3.js
+++ js/sub/menu3.js
@@ -1,8 +1,7 @@
 $('.menutitle3').click(function(){
     $('#menu3').css('display','block');
-    $('#menu1').css('display','none');
-    $('#menu2').css('display','none');
-    $('.menutitle3').css('color','#74a767');
-    $('.menutitle1').css('color','black');
-    $('.menutitle2').css('color','black');
+})
+
+$('#menu3exit i').click(function(){
+    $('#menu3').css('display','none');
 })
